445e5c61662877ba612510919bf03000039560a3
Josh McGhee <joshpmcghee@gmail.com>
2016-05-01 12:37:52 +0100

    move dsl stuff to a dsl file

--- lib/dsl.rb
+++ lib/dsl.rb
@@ -1,3 +1,11 @@
-require 'gadabout'
-
-include Gadabout::DSL
+require 'gadabout/dsl/base'
+require 'gadabout/dsl/job'
+require 'gadabout/dsl/constraint'
+require 'gadabout/dsl/task_group'
+require 'gadabout/dsl/task'
+require 'gadabout/dsl/log_config'
+require 'gadabout/dsl/restart_policy'
+require 'gadabout/dsl/resources'
+require 'gadabout/dsl/artifact'
+require 'gadabout/dsl/network'
+require 'gadabout/dsl/periodic'
